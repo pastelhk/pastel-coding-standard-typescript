c51efc07e33c4a344e4f22f8bae4819b90e4a26a
Jimmy Sinn <jimmyltsinn@gmail.com>
2025-08-27 12:28:14 +0800

    feat(eslint/nestjs): allow dto files to have multiple classes

--- packages/eslint-config-nestjs/recommended.mjs
+++ packages/eslint-config-nestjs/recommended.mjs
@@ -1,3 +1,11 @@
 import node from '@pasteltech/eslint-config-node'
 
-export default [...node.configs.recommended]
+export default [
+  ...node.configs.recommended,
+  {
+    files: ['*.dto.ts'],
+    rules: {
+      'max-classes-per-file': 'off',
+    },
+  },
+]
